2822caf62b4ddbfa473c5f2d2d4f77133b1d1f2a
Geoffrey <gspaur@pronosticgames.fr>
2016-09-24 19:45:49 +0200

    Fix js: selection multiple de fichier

--- PHP_Programmer/IHM_Prototype/test.js
+++ PHP_Programmer/IHM_Prototype/test.js
@@ -1,12 +1,18 @@
 $(document).ready(function(){
 	$selectedFile = null;
     $(".selectFile p").click(function() {
-		$selectedFile = $(this);
+		if ($(this).hasClass("selected")) {
+			$(this).removeClass("selected");
+			$selectedFile = null;
+		} else {
+			$(".selected").removeClass("selected");
+			$(this).addClass("selected");
+			$selectedFile = $(this);
+		}
 		toggleWriteButton();
-		$(this).addClass("selected");
 	});
 	
-	function toggleWriteButton() {
+	function toggleWriteButton() { //hello joris
 		if ($selectedFile == null) {
 			$("#writeButton").addClass("inactive");
 		} else {
