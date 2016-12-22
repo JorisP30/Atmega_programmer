861e4423690923593bf00beab2308bfddb1c9c9c
Geoffrey <geoffrey.spaur@gmail.com>
2016-12-22 11:55:55 +0100

    fix write cmd
    absolute file

--- PHP_Programmer/php/write.php
+++ PHP_Programmer/php/write.php
@@ -1,7 +1,7 @@
 <?php
 include_once("include.php");
 $progName = $urlProgramPy."/".$progPyName["write"];
-$command = "sudo python ".$progName." ".$_POST["args"]." ".$_POST["file"];
+$command = "sudo python ".$progName." ".$_POST["args"]." ".$_SERVER["DOCUMENT_ROOT"].$_POST["file"];
 echo $command;
 $return = shell_exec($command);
 echo $return;
