db1e525d968cf8d3cebd52ffffa66fbca614b414
Geoffrey <geoffrey.spaur@gmail.com>
2016-12-22 12:01:18 +0100

    fix output write

--- PHP_Programmer/php/write.php
+++ PHP_Programmer/php/write.php
@@ -2,7 +2,6 @@
 include_once("include.php");
 $progName = $urlProgramPy."/".$progPyName["write"];
 $command = "sudo python ".$progName." ".$_POST["args"]." ".$_SERVER["DOCUMENT_ROOT"].$_POST["file"];
-echo $command;
 $return = shell_exec($command);
 echo $return;
 ?>
