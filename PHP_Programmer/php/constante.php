0ffbd49e99deb199c5ae9f3b135eb65e4131b550
Geoffrey <geoffrey.spaur@gmail.com>
2016-12-22 11:10:14 +0100

    exec read

--- PHP_Programmer/php/constante.php
+++ PHP_Programmer/php/constante.php
@@ -10,6 +10,12 @@
   $urlSaveAtm = $urlBase."/php/saveAtm.php";
   $urlJs = $urlBase."/js/";
   $urlBaseAtm = $urlBase."/atm";
+  $urlProgramPy = $urlBase."/programmes_python";
+  $progPyName = array(
+    "read" => "lecture_spi.py",
+    "write" => "ecriture_spi.py",
+    "erase" => "chip_erase.py"
+  );
 
   $absolutePathAtm = $_SERVER["DOCUMENT_ROOT"].$urlBaseAtm;
   $urlReadAtmFile = $urlBase."/php/readatmfile.php?folder=files";
@@ -22,13 +28,13 @@
   /* ATM */
   $atmArgs = array(
     "mem" => array(
-      "title" => "Size of flash memory", 
-      "name" => "-m", 
-      "value"=> ""), 
+      "title" => "Size of flash memory",
+      "name" => "",
+      "value"=> ""),
     "nbWord" => array(
-      "title" => "Number of words", 
-      "name" => "-w", 
+      "title" => "Number of words",
+      "name" => "",
       "value" => ""
       )
     );
-?> 
+?>
--- PHP_Programmer/php/read.php
+++ PHP_Programmer/php/read.php
@@ -5,7 +5,13 @@ if (!isset($_POST["atm"]) || empty($_POST["atm"])) {
 }
 date_default_timezone_set('UTC');
 $atm = $_POST["atm"];
+
+$progName = $urlProgramPy."/".$progPyName["read"];
+$command = "python ".$progName." ".$_POST["args"];
+$return = shell_exec($command);
+
 $filename = $atm."/".$atmFolders[1]."/".date("d-m-Y H:i:s");
-file_put_contents($absolutePathAtm."/".$filename, "Retoure de la fonction READ\n");
+file_put_contents($absolutePathAtm."/".$filename, $return);
+echo $command;
 echo "Write result in ".$filename;
-?> 
+?>
